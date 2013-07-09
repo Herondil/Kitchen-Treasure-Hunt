1e9edfb0781bbe8d73ed093dda0a77097635ce0a
Maxens Dubois <maxens.dubois@gmail.com>
2013-07-09 02:43:38 +0200

    Signed-off-by: Maxens Dubois <maxens.dubois@gmail.com>

--- KTH-sources/Assets/Scripts/Scene Manager Scripts/MenuManager.cs
+++ KTH-sources/Assets/Scripts/Scene Manager Scripts/MenuManager.cs
@@ -4,25 +4,102 @@ using System.Collections;
 public class MenuManager : MonoBehaviour {
 	
 	
-	public Rect playButton,leaderboardButton,extraButton;
+	public Rect playJungleButton,
+				playCavernButton,
+				playTempleButton,
+				playPyramidButton,
+				playTitanicButton,
+				leaderboardButton,
+				extraButton;
+	
+	bool drawLeaderBoard,drawExtra;
+	
+	//Extra
+	public Rect resetRect,
+				unlockCavern,
+				unlockTemple,
+				unlockPyramid,
+				unlockTitanic;
+	
+	//Leaderboard
+	public Rect leaderboardRect;
+	
+	//level select
+	public int selGridInt = 0;
+	public Rect levelSelectRect;
+    public string[] selStrings = new string[] {"Jungle", "Cavern", "Temple", "Pyramid", "Titanic"};
 	
 	
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		drawLeaderBoard = false;
+		drawExtra		= false;
+		
+		GUI.backgroundColor = Color.gray;
 	}
 	
 	void OnGUI(){
-		if (GUI.Button(playButton,"Play"))
+		if (GUI.Button(playJungleButton,"Play Jungle"))
 			this.GetComponent<SceneManager>().StartRun();
+		
+		if(PlayerPrefs.GetString("HasUnlockedCavern") == "yes"){
+			if (GUI.Button(playCavernButton,"Play Cavern"))
+				this.GetComponent<SceneManager>().StartRun();
+		}
+		
+		if(PlayerPrefs.GetString("HasUnlockedTemple") == "yes"){
+			if (GUI.Button(playTempleButton,"Play Temple"))
+				this.GetComponent<SceneManager>().StartRun();
+		}
+			
+		if(PlayerPrefs.GetString("HasUnlockedPyramid") == "yes"){
+			if (GUI.Button(playPyramidButton,"Play Pyramid"))
+				this.GetComponent<SceneManager>().StartRun();
+		}
+			
+		if(PlayerPrefs.GetString("HasUnlockedTitanic") == "yes"){
+			if (GUI.Button(playTitanicButton,"Play Titanic"))
+				this.GetComponent<SceneManager>().StartRun();
+		}
+		
+		
+		
 		if (GUI.Button(leaderboardButton,"Leaderboard"))
-            Debug.Log("Clicked the button with text");
+            drawLeaderBoard = !drawLeaderBoard;
 		if (GUI.Button(extraButton ,"Extra"))
-            Debug.Log("Clicked the button with text");
+            drawExtra = !drawExtra;
+		if(drawLeaderBoard) DrawLeaderBoard();
+		if(drawExtra) DrawExtra();
+	}
+	
+	void DrawLeaderBoard(){
+		GUI.Label(leaderboardRect,"LeaderBoard");
+	}
+	
+	void DrawExtra(){
+		//button reset profile
+		if (GUI.Button(resetRect,"Reset Profile"))
+			 PlayerPrefs.DeleteAll();
+		
+		//button unlock level Cavern Temple Pyramid Titanic
+		if(PlayerPrefs.GetString("HasUnlockedCavern") == ""){
+			if (GUI.Button(unlockCavern,"unlockCavern"))
+				PlayerPrefs.SetString("HasUnlockedCavern", "yes");
+		}
+		
+		if(PlayerPrefs.GetString("HasUnlockedTemple") == ""){
+			if (GUI.Button(unlockTemple,"unlockTemple"))
+				PlayerPrefs.SetString("HasUnlockedTemple", "yes");
+		}
+			
+		if(PlayerPrefs.GetString("HasUnlockedPyramid") == ""){
+			if (GUI.Button(unlockPyramid,"unlockPyramid"))
+				PlayerPrefs.SetString("HasUnlockedPyramid", "yes");
+		}
+			
+		if(PlayerPrefs.GetString("HasUnlockedTitanic") == ""){
+			if (GUI.Button(unlockTitanic,"unlockTitanic"))
+				PlayerPrefs.SetString("HasUnlockedTitanic", "yes");
+		}
 	}
 }
