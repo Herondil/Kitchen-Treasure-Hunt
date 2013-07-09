f91b99a3b169a0ca0bbce3e47691e8f33e8c3ad4
Myssdii <myssdii@gmail.com>
2013-07-09 12:20:58 +0200

    Calcul de score

---
+++ KTH-sources/Assets/Scripts/Scene Manager Scripts/CollectibleManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class CollectibleManager : MonoBehaviour {
+	
+	//the asset of the run
+	public GameObject  	exit,
+					  	fourchette;
+	
+	
+	public void GenerateCollectible (Color pixColor, int x, int y) {
+		
+		if(pixColor == Color.red){
+			Instantiate(this.exit, new Vector3(x, 0, y), Quaternion.identity);
+		}
+	}
+	
+	
+}
