19ef01740c686276d7f435a0ede725c54ef13410
Myssdii <myssdii@gmail.com>
2013-07-09 11:44:04 +0200

    Calcul score

--- KTH-sources/Assets/Scripts/Scene Manager Scripts/ScoreManager.cs
+++ KTH-sources/Assets/Scripts/Scene Manager Scripts/ScoreManager.cs
@@ -5,9 +5,17 @@ public class ScoreManager : MonoBehaviour {
 	
 	public Rect scoreRect;
 	
+	ArrayList familles;
+	int points = 0;
+	Hashtable familleCompletion;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
+		familles.Add (new Famille ("Set de table de Kui-Yere-Daur", 10000, new string[3] {"Fourchette de Kui-Yere-Daur", "Assiette de Kui-Yere-Daur", "Couteau de Kui-Yere-Daur"}));
+		familleCompletion["Set de table de Kui-Yere-Daur"] = 0;
+		familles.Add (new Famille ("Poêles jumelles légendaires", 12000, new string[2] {"Poêle à frire de Raie-Ponsse", "Poêle à crèpes de Shande L'Heure"}));
+		familleCompletion["Poêles jumelles légendaires"] = 0;
 	}
 	
 	// Update is called once per frame
@@ -17,5 +25,33 @@ public class ScoreManager : MonoBehaviour {
 	
 	void OnGUI(){
 		GUI.Label(scoreRect,"Score");
+	}
+	
+	public int CalculateTotalPoints (Inventory inventory) {
+		
+		ArrayList items = inventory.GetInventory();
+		
+		for(int i = 0; i < items.Count; i++) {
+			
+			GatheredItem item = (GatheredItem) items[i];
+			
+			points += item.points;
+			
+			foreach(Famille fam in familles) {
+				
+				if(fam.CheckIfFromFamille(item.name)) {
+					
+					int completion = (int) familleCompletion[fam.familleName];
+					familleCompletion[fam.familleName] = completion + 1;
+				}
+				if((int) familleCompletion[fam.familleName] == fam.members.GetLength(0)) {
+					
+					points += fam.bonusPoints;
+				}
+			}
+		}
+		
+		
+		return points;
 	}
 }
